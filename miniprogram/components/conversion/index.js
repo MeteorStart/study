f9b2374f23f07acda58388f71b217993357b54f5
lx802315@163.com <lx802315@163.com>
2021-11-08 18:46:34 +0800

    完成兑换列表页面

---
+++ miniprogram/components/conversion/index.js
@@ -0,0 +1,46 @@
+// components/action/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    state: {
+      type: Boolean,
+      value: false,
+    },
+    srcUrl: {
+      type: String,
+      value: 'https://img.yzcdn.cn/vant/cat.jpeg',
+    },
+    giftName: {
+      type: String,
+      value: '礼物标题',
+    },
+    needIntegral: {
+      type: String,
+      value: '100',
+    },
+    CDKEY: {
+      type: String,
+      value: '1102010021',
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    changed: "已兑换",
+    noChange: "未兑换"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+
+  attached: function () {},
+
+})
