5251689ed6a4a410e666201d2c8dff237c2b1d4e
lx802315@163.com <lx802315@163.com>
2021-11-09 16:25:14 +0800

    完成活动详情页

--- miniprogram/pages/actionDetail/index.js
+++ miniprogram/pages/actionDetail/index.js
@@ -5,7 +5,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        images:['https://img.yzcdn.cn/vant/cat.jpeg','https://img.yzcdn.cn/vant/cat.jpeg','https://img.yzcdn.cn/vant/cat.jpeg']
     },
 
     /**
